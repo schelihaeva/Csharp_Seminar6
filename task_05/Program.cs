99b0763f67f33437e5b39374b8167fadf2eb86b9
shelihaeva <smelkovagolodnova@yandex.ru>
2023-10-22 12:44:15 +0400

    решение задачи про копию массива

--- task_05/Program.cs
+++ task_05/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+using System.Reflection.Metadata.Ecma335;
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for(int i = 0; i < tempArray.Length; i++)
+        tempArray[i] = rand.Next(leftRange, rightRange + 1);
+
+    return tempArray;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] copyArray = new int[array.Length];
+
+    for(int i = 0; i < array.Length; i++)
+        copyArray[i] = array[i];
+
+    return copyArray;
+}
+
+//------------------------------
+int[] myArray = GenerateArray(5, -5, 5);
+PrintArray(myArray);
+
+int[] secondArray = myArray[..]; 
+int[] thirdArray = CopyArray(myArray);
+
+myArray[0] = 100;
+
+PrintArray(myArray);
+PrintArray(secondArray);
+PrintArray(thirdArray);
